856709068e604a5619a8d32cc8ed6d890010f3fa
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-08-27 14:23:00 +0200

    feat: annotate sentence

--- sparv-sbx-sentence-emotional-classification-kb-emoclass/src/sbx_sentence_emotional_classification_kb_emoclass/__init__.py
+++ sparv-sbx-sentence-emotional-classification-kb-emoclass/src/sbx_sentence_emotional_classification_kb_emoclass/__init__.py
@@ -1,6 +1,7 @@
 """Sparv plugin for annotating sentences with emotional classification."""
 
 # from sparv import api as sparv_api
+
 # from sbx_sentence_emotional_classification_kb_emoclass.constants import PROJECT_NAME
 
 __description__ = "Annotate sentences with emotional classification."
